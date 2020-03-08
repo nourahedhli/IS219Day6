364ace3995bcfdef9de49a9f3af5b0517110ef6b
Noura.Hedhli <nh98@njit.edu>
2020-03-08 15:48:45 -0400

    Add: City Model

---
+++ models/city.js
@@ -0,0 +1,24 @@
+
+
+
+class City{
+
+
+    constructor (city, city_ascii,lat,lng,country,iso2,iso3,admin_name,capital,population,id) {
+        this.city = city;
+        this.city_ascii= city;
+        this.lat = lat;
+        this.lng=lng;
+        this.country =country;
+        this.iso2 =iso2;
+        this.iso3 = iso3;
+        this.admin_name=admin_name;
+        this.capital =capital;
+        this.population =population;
+        this.id=id;
+
+    }
+
+
+}
+module.exports = City ;
