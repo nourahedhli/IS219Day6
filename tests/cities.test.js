1afc6564d3673b4b71ccc5633fdfd3564cf2913f
Noura.Hedhli <nh98@njit.edu>
2020-03-09 13:29:03 -0400

    Testing City

--- tests/cities.test.js
+++ tests/cities.test.js
@@ -5,3 +5,13 @@ test('Can create a city object', () => {
     let city = new City();
     expect(city).toBeInstanceOf(City);
 });
+
+
+test ('can create a city object by factory', () => {
+
+    let city = City.create();
+
+
+
+
+})
